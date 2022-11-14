2490d5fc4aa1b505c29582ca9bd2812a218a25ce
percy <percymichael0@gmail.com>
2022-11-14 15:40:11 +0300

    fixed the deploy error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prismadb";
+//import prisma from "../../../lib/prismadb";
 
 export const authOptions = {
   // Configure one or more authentication providers
