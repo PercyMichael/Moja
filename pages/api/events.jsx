dce9afa2b6ca9654ed95fae6e4c126040ff718fd
percy <percymichael0@gmail.com>
2022-12-06 15:58:58 +0300

    added qury4

--- pages/api/events.jsx
+++ pages/api/events.jsx
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
       const client = await clientPromise;
+
       const db = client.db("mojaOneDb");
 
       const movies = await db
