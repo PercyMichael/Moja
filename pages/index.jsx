15852ecffed138c2f547107dc2c65a8db177dc65
percy <percymichael0@gmail.com>
2022-12-06 16:27:32 +0300

    added qury8

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ import RightBar from "../components/RightBar";
 import Nav from "../components/Nav";
 
 export async function getServerSideProps() {
-  const res = await fetch("https://moja.vercel.app/api/events");
+  const res = await fetch(`${NEXT_PUBLIC_HOSTNAME}/api/events`);
   const data = await res.json();
   return { props: { events: data } };
 }
