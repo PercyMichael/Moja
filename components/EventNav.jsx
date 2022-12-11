3b79828a101c41482aeba9b3fdc1b503bc1366d2
Percy <percymichael0@gmail.com>
2022-12-11 21:47:39 +0300

    Neat

--- components/EventNav.jsx
+++ components/EventNav.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const EventNav = () => {
@@ -6,13 +7,15 @@ const EventNav = () => {
     <header className="md:hidden bg-white ">
       <div className="flex justify-between p-6 items-center">
         <div className="">
-          <Image
-            alt="Logo"
-            src={"/vercel.svg"}
-            height={300}
-            width={100}
-            className="h-8"
-          ></Image>
+          <Link href={"/"}>
+            <Image
+              alt="Logo"
+              src={"/vercel.svg"}
+              height={300}
+              width={100}
+              className="h-8"
+            ></Image>
+          </Link>
         </div>
         <div className=" bg-blue-400 text-white uppercase text-sm items-center p-2 rounded-xl font-bold cursor-pointer">
           Tickets
--- components/singleEvent/EventDetail.jsx
+++ components/singleEvent/EventDetail.jsx
@@ -4,10 +4,16 @@ import EventNav from "../EventNav";
 
 const EventDetail = (props) => {
   return (
-    <main className="bg-gray-100 w-full h-full md:border-x">
+    <main className="w-full h-full md:border-x">
       <EventNav />
-      <div className="event-detail pb-30 lg:gap-x-9 gap-y-8 md:max-h-screen md:overflow-auto">
-        <Image src={"/bike.jpg"} height={400} width={600} alt="event poster" />
+      <div className="event-detail bg-white pb-30 lg:gap-x-9 gap-y-8 md:max-h-screen md:overflow-auto">
+        <Image
+          className=" rounded-b-2xl"
+          src={"/bike.jpg"}
+          height={400}
+          width={600}
+          alt="event poster"
+        />
       </div>
     </main>
   );
