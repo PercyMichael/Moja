a835d449fa5983b1111925c75c43fb117a71dc69
Percy <percymichael0@gmail.com>
2022-12-27 04:36:20 +0300

    margin

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Stats = () => {
   return (
     <>
-      <div className="flex text-white justify-between align-middle m-3 py-7 px-3 shadow rounded-xl bg-[#7742db]">
+      <div className="flex text-white justify-evenly align-middle m-3 py-7 px-3 shadow rounded-xl bg-[#7742db]">
         <div id="tickets-bought" className="flex flex-col items-center">
           <div className=" font-bold text-2xl">4,200</div>
           <div>Tickets Bought</div>
         </div>
-        <div id="tickets-sold" className="flex flex-col items-center">
-          <div className=" font-bold text-2xl">4,200</div>
-          <div>Tickets Sold</div>
-        </div>
         <div id="events" className="flex flex-col items-center">
           <div className=" font-bold text-2xl">4,200</div>
           <div>Events</div>
+        </div>
+        <div id="tickets-sold" className="flex flex-col items-center">
+          <div className=" font-bold text-2xl">4,200</div>
+          <div>Tickets Sold</div>
         </div>
       </div>
     </>
