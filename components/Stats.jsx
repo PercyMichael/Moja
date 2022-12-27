f90ab1dd8e5646961135b920bfcd2fa3a5e4cbe8
Percy <percymichael0@gmail.com>
2022-12-27 03:22:14 +0300

    menu enhance

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -5,15 +5,15 @@ const Stats = () => {
     <>
       <div className="flex justify-between align-middle bg-white m-3 p-4">
         <div id="tickets-bought" className="">
-          <div className="stat-value text-[#7742db]">4,200</div>
+          <div className="text-[#7742db] font-bold text-2xl">4,200</div>
           <div className="stat-title">Tickets Bought</div>
         </div>
         <div id="tickets-sold">
-          <div className="stat-value text-[#7742db]">4,200</div>
+          <div className=" text-[#7742db] font-bold text-2xl">4,200</div>
           <div className="stat-title">Tickets Sold</div>
         </div>
         <div id="events">
-          <div className="stat-value text-[#7742db]">4,200</div>
+          <div className=" text-[#7742db] font-bold text-2xl">4,200</div>
           <div className="stat-title">Events</div>
         </div>
       </div>
