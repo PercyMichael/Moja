34b786fa1a95d63d354e3d92dc560902cce4e72e
Percy <percymichael0@gmail.com>
2022-12-27 05:05:41 +0300

    aded icons

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-
+import { HiTicket, HiOutlineTicket } from "react-icons/hi2";
+import { SlEvent } from "react-icons/sl";
 const Stats = () => {
   return (
     <>
       <div className="flex text-white justify-evenly align-middle mx-3 my-11 py-7 px-3 shadow rounded-xl bg-[#7742db]">
         <div id="tickets-bought" className="flex flex-col items-center">
+          <HiTicket className="text-2xl" />
           <div className=" font-bold text-2xl">4,200</div>
+
           <div>Tickets Bought</div>
         </div>
         <div id="events" className="flex flex-col items-center">
+          <SlEvent className="text-2xl" />
           <div className=" font-bold text-2xl">4,200</div>
           <div>Events</div>
         </div>
         <div id="tickets-sold" className="flex flex-col items-center">
+          <HiOutlineTicket className="text-2xl" />
           <div className=" font-bold text-2xl">4,200</div>
           <div>Tickets Sold</div>
         </div>
