7e60758c985f4943fda12130ddb3b08286a748cc
Percy <percymichael0@gmail.com>
2022-12-27 04:25:13 +0300

    margin

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Stats = () => {
   return (
     <>
-      <div className="flex justify-between align-middle bg-white m-3 py-4 px-3">
+      <div className="flex justify-between align-middle bg-white m-3 py-7 px-3">
         <div id="tickets-bought" className="flex flex-col items-center">
           <div className="text-[#7742db] font-bold text-2xl">4,200</div>
           <div className="stat-title">Tickets Bought</div>
--- pages/user/dashboard.jsx
+++ pages/user/dashboard.jsx
@@ -11,14 +11,15 @@ const dashborad = () => {
       <main className="w-full h-full md:border-x">
         <Nav />
         <Stats />
-        <div id="my-tickets" className=" px-6">
-          <div id="top-nav" className="flex w-full justify-between">
+        <div id="my-tickets">
+          <div id="top-nav" className="flex w-full justify-between px-6">
             <h6>Recent Tickets</h6>
             <h6 className="text-sm text-gray-500">See All</h6>
           </div>
+          {/*tickets*/}
           <div
             id="ticket"
-            className="py-3 flex justify-between items-center border-b"
+            className="py-3 flex justify-between items-center border-b px-6"
           >
             <div className="flex flex-col">
               <h4 className="text-lg font-bold">Hello</h4>
@@ -28,7 +29,7 @@ const dashborad = () => {
           </div>
           <div
             id="ticket"
-            className="py-3 flex justify-between items-center border-b"
+            className="py-3 flex justify-between items-center border-b px-6"
           >
             <div className="flex flex-col">
               <h4 className="text-lg font-bold">Hello</h4>
@@ -38,7 +39,7 @@ const dashborad = () => {
           </div>
           <div
             id="ticket"
-            className="py-3 flex justify-between items-center border-b"
+            className="py-3 flex justify-between items-center border-b px-6"
           >
             <div className="flex flex-col">
               <h4 className="text-lg font-bold">Hello</h4>
@@ -48,7 +49,7 @@ const dashborad = () => {
           </div>
           <div
             id="ticket"
-            className="py-3 flex justify-between items-center border-b"
+            className="py-3 flex justify-between items-center border-b px-6"
           >
             <div className="flex flex-col">
               <h4 className="text-lg font-bold">Hello</h4>
@@ -58,7 +59,7 @@ const dashborad = () => {
           </div>
           <div
             id="ticket"
-            className="py-3 flex justify-between items-center border-b"
+            className="py-3 flex justify-between items-center border-b px-6"
           >
             <div className="flex flex-col">
               <h4 className="text-lg font-bold">Hello</h4>
@@ -66,6 +67,17 @@ const dashborad = () => {
             </div>
             <h4 className="text-medium font-bold text-[#7742db]">UGX 20,000</h4>
           </div>
+          <div
+            id="ticket"
+            className="py-3 flex justify-between items-center border-b px-6"
+          >
+            <div className="flex flex-col">
+              <h4 className="text-lg font-bold">Hello</h4>
+              <div className="text-sm text-gray-500">Expired . 40 days ago</div>
+            </div>
+            <h4 className="text-medium font-bold text-[#7742db]">UGX 20,000</h4>
+          </div>
+          {/*end tickets*/}
         </div>
       </main>
     </div>
