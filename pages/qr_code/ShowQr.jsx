d78b70c151ac706897ea8f11501e353811626e33
Percy <percymichael0@gmail.com>
2023-01-02 20:26:57 +0300

    ticket

--- pages/qr_code/ShowQr.jsx
+++ pages/qr_code/ShowQr.jsx
@@ -18,9 +18,18 @@ const ShowQr = () => {
           alt="event poster"
         />
 
-        <div className="bg-[#7742db] p-6">
-          <h1 className="font-medium text-lg text-white py-3">Event Title</h1>
-          <QRCodeSVG value="a" bgColor={"#FFf"} fgColor={"#7742db"} />
+        <div className=" p-6 flex flex-col items-center shadow-md rounded-xl">
+          <h1 className="font-medium text-lg text-[#7742db] py-3">
+            Event Title
+          </h1>
+          <QRCodeSVG
+            className="border-8 border-white"
+            value="a"
+            width={300}
+            height={300}
+            bgColor={"#FFf"}
+            fgColor={"#7742db"}
+          />
         </div>
       </div>
       {/*end qr*/}
