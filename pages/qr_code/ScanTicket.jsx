c7528ddf4afede6c8858c1f28773c34f3d62c0f3
percy <percymichael0@gmail.com>
2023-01-11 10:18:32 +0300

    try scanninh

--- pages/qr_code/ScanTicket.jsx
+++ pages/qr_code/ScanTicket.jsx
@@ -1,33 +1,11 @@
-import React, { useState } from "react";
-import QrReader from "react-qr-scanner";
+import React from "react";
+import QRScan from "../../components/QrScan";
 
-const ScanTicket = () => {
-  const [qrscan, setQrscan] = useState("No result");
-
-  const handleScan = (data) => {
-    if (data) {
-      setQrscan(data);
-    }
-  };
-  const handleError = (err) => {
-    console.error(err);
-  };
-
+export default function App() {
   return (
-    <div>
-      <center>
-        <div style={{ marginTop: 30 }}>
-          <QrReader
-            delay={300}
-            onError={handleError}
-            onScan={handleScan}
-            style={{ height: 240, width: 320 }}
-          />
-        </div>
-        <h1>{qrscan}</h1>
-      </center>
+    <div className="App">
+      <h1>Hello QR Code Reader</h1>
+      <QRScan />
     </div>
   );
-};
-
-export default ScanTicket;
+}
